1b7baf3160e3cfd08653d0f2fa52deb330833208
Your Name”git config --global user.email you@example.com  git config --global user.name Your <you@example.com>
2022-01-26 00:39:00 +0300

    Updated.

---
+++ githubtask.sh
@@ -0,0 +1,49 @@
+#! /bin/zsh
+# Filza da kullanmak için, Terminali normal (nonSuperuser) olarak #ayarlayın.
+#=====================# Renklendirme :)
+Y='\033[1;33m' #=Sarı=# 
+#=====================# 
+clear
+echo "Updating Repo(s)"
+
+cd /private/var/mobile/Documents/Github/home
+git pull
+git add /private/var/mobile/Documents/Github/home
+cd /private/var/mobile/Documents/Github/repo
+git pull
+git add /private/var/mobile/Documents/Github/repo
+cd /private/var/mobile/Documents/Github/secretrepo
+git pull
+git add /private/var/mobile/Documents/Github/secretrepo
+
+echo "Updated"
+
+clear
+
+echo "Commit changes"
+
+cd /private/var/mobile/Documents/Github/secretrepo
+git commit -a -m "Updated." 
+cd /private/var/mobile/Documents/Github/repo
+git commit -a -m "Updated."
+cd /private/var/mobile/Documents/Github/home
+git commit -a -m "Updated."
+
+echo "Comitted."
+
+clear
+
+echo "${Y}Upload started."
+
+cd /private/var/mobile/Documents/Github/secretrepo
+git push
+cd /private/var/mobile/Documents/Github/repo
+git push
+cd /private/var/mobile/Documents/Github/home
+git push 
+
+echo "Upload Completed"
+
+clear 
+
+tput setaf 2;echo "Success"
