698a6d3caa666c8343a1adcedce682c196638688
Turann Ul <ulutast2214@gmail.com>
2022-01-27 04:48:40 +0300

    Updated.

--- githubtask.sh
+++ githubtask.sh
@@ -3,6 +3,58 @@
 #=====================# Renklendirme :)
 Y='\033[1;33m' #=Sarı=# 
 #=====================# 
+#Determine device 
+test=$(uname -n)
+if [[ $test == Turanns-iMac ]]; then
+	echo "$test"
+	clear
+echo "Updating Repo(s)"
+
+cd /Users/turannul/Documents/Github/home
+git pull
+git add /Users/turannul/Documents/Github/home
+cd /Users/turannul/Documents/Github/repo
+git pull
+git add /Users/turannul/Documents/Github/repo
+cd /Users/turannul/Documents/Github/secretrepo
+git pull
+git add /Users/turannul/Documents/Github/secretrepo
+
+echo "Updated"
+
+clear
+
+echo "Commit changes"
+
+cd /Users/turannul/Documents/Github/secretrepo
+git commit -a -m "Updated." 
+cd /Users/turannul/Documents/Github/repo
+git commit -a -m "Updated."
+cd /Users/turannul/Documents/Github/home
+git commit -a -m "Updated."
+
+echo "Comitted."
+
+clear
+
+echo "${Y}Upload started."
+
+cd /Users/turannul/Documents/Github/secretrepo
+git push
+cd /Users/turannul/Documents/Github/repo
+git push
+cd /Users/turannul/Documents/Github/home
+git push 
+
+echo "Upload Completed"
+
+clear 
+
+tput setaf 2;echo "Success"
+
+else
+	#if [[ $test == Turanns-iPhone ]]; then		
+#echo "$test"
 clear
 echo "Updating Repo(s)"
 
@@ -47,3 +99,5 @@ echo "Upload Completed"
 clear 
 
 tput setaf 2;echo "Success"
+fi
+  #fi
