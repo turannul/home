61742b50520d6218dab8e6146cf62dd97de9e955
Turann Ul <ulutast2214@gmail.com>
2022-01-27 05:00:40 +0300

    Updated.

--- gitupdate-mac.sh
+++ gitupdate-mac.sh
@@ -1,2 +1,4 @@
+#! /opt/procursus/bin/zsh
+chmod +x /Users/turannul/gitupdate-mac.sh
 chmod +x /Users/turannul/Documents/GitHub/home/githubtask.sh 
 /Users/turannul/Documents/GitHub/home/githubtask.sh 
