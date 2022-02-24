8f532b4ce9617b5f5a5d07f4106f660cc5d47afe
turannul <turannul000@gmail.com>
2022-02-24 14:08:38 +0300

    Updated.

--- commit.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-# Filza da kullanmak için, Terminali normal (nonSuperuser) olarak #ayarlayın.
-cd /private/var/mobile/Documents/home
-git pull
-git add /private/var/mobile/Documents/home
-git commit -a -m "New commit from mobile" 
-git push
