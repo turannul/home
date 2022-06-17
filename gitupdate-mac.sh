d529117482e3a430121585a6de309e274d2399d6
Turann <nonameconfirmed7@gmail.com>
2022-06-17 18:14:17 +0300

    Delete gitupdate-mac.sh

--- gitupdate-mac.sh
+++
@@ -1,4 +0,0 @@
-#! /opt/procursus/bin/zsh
-chmod +x /Users/turannul/gitupdate-mac.sh
-chmod +x /Users/turannul/Documents/GitHub/home/githubtask.sh 
-/Users/turannul/Documents/GitHub/home/githubtask.sh 
